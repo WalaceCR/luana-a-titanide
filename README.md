1ad24e9bba8c3450cbad1747e96e15264c172aba
WalaceCR <walace.dacunha@yahoo.com.br>
2019-12-29 12:33:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Luana-a-titanide
+Projeto de extração de dados da internet para Excel utilizando Python + Scrapping + Selenium.
+
+# Rodando o projeto
+Já possuindo o python e o gerenciador de pacotes pip na máquina é necessário baixar as dependências do projeto
+com os comandos abaixo no CMD:
+
+pip install -r requirements.txt
+
+Baixadas todas as dependências para rodar o projeto basta rodar o comando abaixo ainda na pasta do projeto:
+
+python minemosine.py
+
+# Resultado do projeto
+Resultado do projeto será um csv com os arquivos capturados dentro da pasta do projeto.
---
+++ minemosine.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from dados.config import settings
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+import time
+import pandas as pd
+import os
+
+# Colocar isso em uma classe SuporteAutomato
+driverlocation = r"chromedriver"
+os.environ["webdriver.chrome.driver"]=driverlocation
+driver = webdriver.Chrome()
+
+# Colocar isso em uma classe PageObject ou algo do tipo
+coluna_1=[]
+coluna_2=[]
+coluna_3=[]
+coluna_4=[]
+coluna_5=[]
+coluna_6=[]
+
+driver.get(settings['url'])
+
+driver.maximize_window()
+
+txt_usuario = driver.find_element(By.ID, settings['elemento-usuario-id'])
+txt_senha = driver.find_element(By.ID, settings['elemento-senha-id'])
+btn_button = driver.find_element(By.ID, settings['elemento-botao-id'])
+
+txt_usuario.send_keys(settings['usuario'])
+txt_senha.send_keys(settings['senha'])
+btn_button.click()
+
+delay = 25 # seconds
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, settings['elemento-ancora'])))
+    print("Página está pronta!!! :)")
+except TimeoutException:
+    print("Esperei demais :(")
+
+content = driver.page_source
+soup = BeautifulSoup(content, "lxml")
+
+tables = soup.find("table", {"class": settings['classe-tabela']}).find("tbody")
+
+for tr in tables.findAll("tr"):
+    td = tr.findAll("td")
+    if len(td) == 6:
+        coluna_1.append(td[0].text)
+        coluna_2.append(td[1].text)
+        coluna_3.append(td[2].text)
+        coluna_4.append(td[3].text)
+        coluna_5.append(td[4].text)
+        coluna_6.append(td[5].text)
+    print("Capturando dados da página...")
+    
+print("Copiando dados para o Excel...")
+df = pd.DataFrame({settings['cabecalho-1']:coluna_1,settings['cabecalho-2']:coluna_2,settings['cabecalho-3']:coluna_3,settings['cabecalho-4']:coluna_4,settings['cabecalho-5']:coluna_5,settings['cabecalho-6']:coluna_6}) 
+df.to_csv(settings['titulo-arquivo-gerado'], index=False, encoding=settings['encoding'])
+print("Excel gerado com sucesso!!!")
+
+driver.delete_all_cookies()
+driver.quit()
